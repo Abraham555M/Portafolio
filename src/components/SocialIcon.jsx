65d5b9d16470409d3f09738955b2122914e4f13b
abraham <abraham2004hf@gmail.com>
2025-07-21 14:54:08 -0500

    Cambios

--- src/components/SocialIcon.jsx
+++ src/components/SocialIcon.jsx
@@ -1,12 +1,28 @@
-export const SocialIcon = ({ href, children, color = "green-500", size = "text-2xl" }) => (
-  <a
-    href={href}
-    target="_blank"
-    rel="noopener noreferrer"
-    className={`group p-3 rounded-full bg-white/10 hover:bg-${color} transition-all duration-300`}
-  >
-    <div className={`${size} text-white group-hover:text-white transition-all duration-300`}>
-      {children}
-    </div>
-  </a>
-);
+// SocialIcon.jsx
+import PropTypes from "prop-types";
+import clsx from "clsx";
+
+export const SocialIcon = ({ href, children, color = "hover:bg-pink-500", size = "text-2xl" }) => {
+  return (
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={clsx(
+        "group p-4 rounded-full bg-white transition-all duration-300",
+        color
+      )}
+    >
+      <div className={clsx(size, "text-black group-hover:text-white transition-all duration-300")}>
+        {children}
+      </div>
+    </a>
+  );
+};
+
+SocialIcon.propTypes = {
+  href: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+  color: PropTypes.string,
+  size: PropTypes.string,
+};
--- src/sections/Inicio/Inicio.jsx
+++ src/sections/Inicio/Inicio.jsx
@@ -6,11 +6,11 @@ import { FaReact, FaLaravel, FaAndroid } from "react-icons/fa";
 import { SiMysql } from "react-icons/si";
 import TypedText from '../../components/TypedText';
 import { MdWavingHand } from "react-icons/md";
+import { MdWork } from 'react-icons/md';
 
 export const Inicio = () => {
   return (
-      <section id="home" className="text-white flex flex-col lg:flex-row"
-
+      <section id="home" className="text-white flex flex-col lg:flex-row "
         /*
         style={{
           backgroundImage: `url(${fondo})`,
@@ -18,62 +18,85 @@ export const Inicio = () => {
         */
       >
       {/* Sección de informacion */}
-      <div className="w-full lg:w-[60%] p-10 lg:p-20 flex flex-col justify-center gap-7">
-      {/* Parte 1 */}        
-      <motion.div
-        initial={{ opacity: 0, x: -50 }}
-        whileInView={{ opacity: 1, x: 0 }}
-        transition={{ duration: 0.8 }}
-      >          
-      <h1 className="font-bold text-xl sm:text-2xl">HELLO I'M</h1>        
-      <h2 className="mt-5 mb-2 font-bold text-3xl sm:text-4xl lg:text-5xl">
-        <span>Abraham Manuel</span><br />
-        <span className="flex items-center mt-2">
-          Hilario Fernández
-          <MdWavingHand className="text-yellow-500 text-4xl ml-4 mt-2 rotate-360" />
-        </span>
-      </h2>
-      <TypedText text="Full Stack Developer & Android Developer" />
-      </motion.div>
+      <div className="w-full lg:w-[60%] p-10 lg:p-20 flex flex-col justify-center gap-10">
+        {/* Parte 1 */}        
+        <motion.div
+          className=''
+          initial={{ opacity: 0, x: -50 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.8 }}
+        >          
+        <h1 className="font-bold text-xl sm:text-2xl">HELLO I'M</h1>        
+        <h2 className="mt-5 mb-2 font-bold text-3xl sm:text-4xl lg:text-5xl">
+          <span>Abraham Manuel</span><br />
+          <span className="flex items-center mt-2">
+            Hilario Fernández
+            <MdWavingHand className="text-yellow-500 text-4xl ml-4 mt-2 rotate-360" />
+          </span>
+        </h2>
+        <TypedText text="Full Stack Developer & Android Developer" />
+        </motion.div>
 
         {/* Parte 2 */}
+        <div className=''>
+          <h2 className="text-xl font-bold text-white">
+            Quick Stats
+          </h2>
+          <div className='flex gap-5 mt-2'>
+          <div className="bg-white w-40 rounded-3xl p-3 text-black text-center font-semibold flex gap-2 transition-transform duration-300 hover:scale-105 cursor-pointer">
+              <MdWork className="text-black text-2xl" />
+              <p>+1 Experience</p>
+            </div>
+            <div className="bg-white w-40 rounded-3xl p-3 text-black text-center font-semibold flex gap-2 transition-transform duration-300 hover:scale-105 cursor-pointer">
+              <MdWork className="text-black text-2xl" />
+              <p>+1 Experience</p>
+            </div>
+            <div className="bg-white w-40 rounded-3xl p-3 text-black text-center font-semibold flex gap-2 transition-transform duration-300 hover:scale-105 cursor-pointer">
+              <MdWork className="text-black text-2xl" />
+              <p>+1 Experience</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Parte 3 */}
         <motion.div
-          className="font-mono mt-5"
+          className="font-mono "
           initial={{ opacity: 0, x: -100 }}
           whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.8, delay: 0.4 }}
         >
-          <h1 className="text-3xl font-semibold underline text-green-600">
+          <h1 className="text-2xl font-semibold underline text-green-600">
             abraham2004hf@gmail.com
           </h1>
-          <p className="font-semibold text-2xl">
+          <p className="font-semibold text-xl">
             Lima, Puente Piedra <br /> Peru
           </p>
         </motion.div>
 
-        {/* Parte 3 */}
+        {/* Parte 4 */}
         <motion.div
-          className="mt-15 flex gap-4"
+          className=""
           initial={{ opacity: 0, scale: 0.8 }}
           whileInView={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.8, delay: 0.8 }}
         >
-          <SocialIcon href="https://www.linkedin.com/in/abraham-manuel-hilario-fernández-a04731282/" color="blue-500">
-            <FaLinkedin className="text-white text-2xl group-hover:text-white transition-all duration-300" />
-          </SocialIcon>
-
-          <SocialIcon href="https://github.com/Manuelh2004" color="green-500">
-            <FaGithub className="text-white text-2xl group-hover:text-white transition-all duration-300" />
-          </SocialIcon>
-
-          <SocialIcon href="https://www.instagram.com/tu_usuario" color="pink-500">
-            <FaInstagram className="text-white text-2xl group-hover:text-white transition-all duration-300" />
-          </SocialIcon>
+          <h2 className="text-xl font-bold text-white"> Follow me </h2>
+          <div className='flex gap-4 mt-2'>
+            <SocialIcon href="https://www.instagram.com/tu_usuario" color="hover:bg-blue-500">
+              <FaLinkedin/>
+            </SocialIcon>
+            <SocialIcon href="https://www.instagram.com/tu_usuario" color="hover:bg-green-500">
+              <FaGithub/>
+            </SocialIcon>
+            <SocialIcon href="https://www.instagram.com/tu_usuario" color="hover:bg-pink-500">
+              <FaInstagram/>
+            </SocialIcon>
+          </div>
         </motion.div>
       </div>
-
+      
       {/* Sección de Iconos */}
-      <div className="w-full lg:w-[40%] flex flex-wrap justify-center items-center gap-6 p-6 border">
+      <div className="w-full lg:w-[40%] flex flex-wrap justify-center items-center gap-6 p-6 ">
         {/* React */}
         <motion.div
           animate={{ y: [0, -20, 0] }}
@@ -89,7 +112,7 @@ export const Inicio = () => {
           animate={{ y: [0, -20, 0] }}
           transition={{ duration: 2.2, repeat: Infinity, ease: "easeInOut" }}
           whileHover={{ scale: 1.2 }}
-          className="inline-block rounded-full bg-black p-5"
+          className="inline-block rounded-full bg-white text-black p-5"
         >
           <FaLaravel className="text-2xl cursor-pointer" />
         </motion.div>
